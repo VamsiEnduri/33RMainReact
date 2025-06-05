ca518a198093119de431d71b5d636fedb9aabf02
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-06-05 16:04:19 +0530

    virtualDom

--- conditional_rendering/src/App.jsx
+++ conditional_rendering/src/App.jsx
@@ -1,18 +1,79 @@
-import React from 'react'
-import Login from './Login'
-const App = () => {
-  let signUpEmail=prompt("enter signup email")
-  let signUpPswd=prompt("enter signup pswd")
-  let signUpRole=prompt("enter signup role")
+// import React from 'react'
+// import Login from './Login'
+// const App = () => {
+//   let signUpEmail=prompt("enter signup email")
+//   let signUpPswd=prompt("enter signup pswd")
+//   let signUpRole=prompt("enter signup role")
 
-  const usersFromLS=JSON.parse(localStorage.getItem("users")) || []
-  usersFromLS.push({signUpEmail,signUpPswd,signUpRole})
-  localStorage.setItem("users",JSON.stringify(usersFromLS))
+//   const usersFromLS=JSON.parse(localStorage.getItem("users")) || []
+//   usersFromLS.push({signUpEmail,signUpPswd,signUpRole})
+//   localStorage.setItem("users",JSON.stringify(usersFromLS))
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+
+// const App = () => {
+//   const isLoggedIn=true
+//   return (
+//     <div>
+//       {isLoggedIn ? <DashBoard /> :<Login />}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+
+// const App = () => {
+//   const isElgibleTovOte=19;
+//   return (
+//     <div>
+//       {isElgibleTovOte >=18 ? <Eligible /> :<NotEligible />}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+
+// const App = () => {
+//   const isSteelBottle=false;
+//   return (
+//     <div>
+//       {isSteelBottle ? <SteelBottle /> :<PlasticBottle />}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
   return (
     <div>
-      <Login />
+      <h1>hello</h1>
+      <p>p tag Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quidem perferendis quam non sint culpa soluta veniam aperiam modi laborum.</p>
+      <button>btn tag</button>
+      <p>hellooooo</p>
     </div>
   )
 }
+console.log(App(),"app")
 
 export default App
+
---
+++ qna.js
@@ -0,0 +1,82 @@
+// what is react ?
+ // react is js library which is open source and can be used to create rich user UIs based on comp architecture
+
+ //what is library ?
+ // a lib is notng collection of predefined functions r code
+
+//  what are features of react ?
+//  comp reusability
+// one way data flow ( prop drilling )
+//  virtual DOM
+// jsx
+// declarative UI
+
+// what is comp? and types of comps?
+//  comp is nothing but a js function which returns jsx
+// and comps are 2 types :--1 . function based comps 2.. class based comps
+
+//what is jsx?
+//  jsx is nothing but javascript XML and it resmebles html like strcture 
+// jsx is nothibg but html like structrure
+
+//what are props in react? and types of props?
+// props are data carriers which carries data from parent comp to child comp
+// and props are 2 types :-- 1.general props 2.child props
+
+
+// what is prop drilling ?
+// the way of data flow among comps in hierachical manner 
+
+// what is list rendering in react and give an example ?
+// list rendering is nothing but displaying list of items r array of ietms and for tos we need to use map method and we need to display the content on ui as jsx
+
+//what is conditional rendering ? give an example?
+ // conditional rendering is nothing but renderig the comps based on certain conditions
+
+
+ // how can you do routing in react ?
+ // for routing in react, we need to install a third party routing pacakge r libraray of react into react project and we can install that byusing npm i react-router-dom command
+
+ // explain me about comps in RRD?
+//  BrowserRouter :-- which enables routing to entire react app
+// Routes :-- it is used to warp up all single route
+//  Route :-- which has path and elem attribites in path we need to give route and in elem we need to give syncable comp to that matching path
+// Link :-- it is a comp which is used to set the destination route with TO attribute
+{/* <Link to="/login">Login</Link> */}
+
+// explain me how yoy can install react project with vite?
+// create a folder :-- node -v, npm -v
+// command :-- npm create vite@latest medicohospital
+//  selct react + select js
+// cd react_app_name :-- cd medicohospital
+// npm i :-- neccessary all packages for react+vite app runing
+// npm run dev :-- 
+
+
+// what is vitaulDOM?
+//  it is copy of realDOM
+
+// is yr browser can understands jsx directly? NO
+
+
+//babel?
+// it is a transpiler which transplies yr jsx code to nrml js objects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// how yr react application getting rendered on screen ?
